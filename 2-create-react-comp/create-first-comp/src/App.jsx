214e95d2de967a5b584236cb9f2570be5939afb8
Jayadev Bemal <jugarajbemal@gmail.com>
2026-02-07 18:11:55 +0530

    react learning

---
+++ 2-create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,10 @@
+import {Btn} from "./button";
+function App(){
+
+  return <h1>
+    Odisha-Pulse <h2> <Btn></Btn> </h2>
+  </h1>
+  
+}
+
+export default App;
---
+++ 2-create-react-comp/create-first-comp/src/button.jsx
@@ -0,0 +1,3 @@
+export function Btn(){
+  return <button>Vote</button>
+}
