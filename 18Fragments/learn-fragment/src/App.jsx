4b9e454b4609772c6072f509a90f136b8a356246
Jayadev Bemal <jugarajbemal@gmail.com>
2026-02-11 21:54:13 +0530

    component and styling practice

--- 18Fragments/learn-fragment/src/App.jsx
+++ 18Fragments/learn-fragment/src/App.jsx
@@ -4,18 +4,23 @@ import "./App.css";
 import Listitems from "./Listitems";
 import Cndt from "./Varstore";
 import Item from "./Item";
+import Container from "./Container";
 
 
 function App() {
 let foodItems = ['Banana','Apple','Mango','Orange','Pine-Apple'];
 
-  return (
-    <React.Fragment>
+  return (<>
+    <Container>
       <h1 className="jb-heading">Healthy Fruits</h1>
       {<Cndt items = {foodItems}></Cndt>}
       <Listitems items = {foodItems}></Listitems>
-    </React.Fragment>
-  );
+    </Container>
+
+    <Container>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est modi consequuntur, debitis, cum vel, maxime quia aliquam cupiditate enim delectus ratione? Dolorum, delectus! Explicabo odio accusantium quos quod temporibus obcaecati libero ex, nam nostrum corrupti? Quaerat incidunt qui pariatur dolore!</p>
+    </Container>
+  </>);
 }
 
 export default App;
--- 18Fragments/learn-fragment/src/Item.jsx
+++ 18Fragments/learn-fragment/src/Item.jsx
@@ -1,9 +1,17 @@
 import styles from './Item.module.css'
+import Listitems from './Listitems'
+
+
 
 const Item = ({Listitems}) => {
-console.log(styles)
-  return <li className= {`${styles["jb-item"]} list-group-item`} >
-          <span className={`${styles["jb-span"]}`}>{Listitems}</span>
-        </li> 
+
+const ButtonClick = () => {
+  alert(`${Listitems} bought`)
 }
+  return <li className= {`${styles["jb-item"]} list-group-item`} >
+            <span className={`${styles["jb-span"]}`}>{Listitems}</span> <button className={`btn btn-info ${styles.btnBh}`} onClick={ButtonClick}>Buy</button>
+        </li> 
+    
+} 
+
 export default Item;
