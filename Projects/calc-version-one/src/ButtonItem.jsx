6ad46992034a45ba1cb06a2636a76e289aa63354
Jayadev Bemal <jugarajbemal@gmail.com>
2026-03-07 11:59:24 +0530

    calculator final version

--- Projects/calc-version-one/src/ButtonItem.jsx
+++ Projects/calc-version-one/src/ButtonItem.jsx
@@ -5,7 +5,7 @@ let buttonArray= ['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0
 
 
   return <div>{buttonArray.map((i) => 
-    <button type="button" key={i} className={`btn btn-light ${styles.btn}`} >{i}</button>
+    <button type="button" key={i} className={`btn btn-light ${styles.btn}` } >{i}</button>
   )}
        
       </div> 
